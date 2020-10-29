0276ea683079dbbbf2824fb84cb2ced11ab7d9c9
Manon <ballatoremanon@gmail.com>
2020-10-29 14:15:41 +0100

    ajout de 11 legumes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 var poeme = document.getElementById('poeme').textContent;
 var compteur = document.getElementById('compteur');
 compteur.innerHTML = poeme.length;
+
+var legumes = ["patate", "aubergine","carotte", "haricot", "courgette", "citrouille", "lentille", "salades", "chou-fleur", "poivron","betterave"]
