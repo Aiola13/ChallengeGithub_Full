5e5ce5590fe08647642f25c9345648ce0d6c25b4
System Administrator <root@MacBook-Pro.local>
2020-10-29 15:17:20 +0100

    ajout legumes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 var poeme = document.getElementById('poeme').textContent;
 var compteur = document.getElementById('compteur');
 compteur.innerHTML = poeme.length;
+var legumes = ["carotte", "patate", "choux", "poivron", "lentilles", "salades", "courgette", "epinards", "aubergine", "betteraves", "haricots" ];
+console.log(legumes)
