1c35ab30dea5488b9be57a7ac87746a7bc50cf25
pikilla <maite.lajournade@gmail.com>
2020-10-29 16:26:43 +0100

    tableau

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 var poeme = document.getElementById('poeme').textContent;
 var compteur = document.getElementById('compteur');
 compteur.innerHTML = poeme.length;
+var legumes = ["aubergine", "concombre" , "chou" , "rutabaga" , "courgette" , "potiron" , "butternut", "poivron", "haricot", "carotte" , "oignon"];
+console.log(legumes[0]);
+console.log(legumes[10]);
+
